b9cfd4d22a92682832a25898cca86191871808d5
JoshJoseph1234 <joshjosephcod@gmail.com>
2025-02-22 14:12:36 +0530

    Removed unwanted files

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import os
 import platform
-import string  # Add this import
+import string
 import json
 
 app = Flask(__name__)
@@ -46,26 +46,3 @@ def get_files(directory):
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
-from flask import Flask
-from controllers.file_controllers import FileController
-
-app = Flask(__name__)
-
-# Initialize the FileController
-file_controller = FileController()
-
-# Define routes
-@app.route('/files', methods=['GET'])
-def get_files():
-    return file_controller.get_files()
-
-@app.route('/files/categorize', methods=['POST'])
-def categorize_files():
-    return file_controller.categorize_files()
-
-@app.route('/files/sort', methods=['POST'])
-def sort_files():
-    return file_controller.sort_files()
-
-if __name__ == '__main__':
-    app.run(debug=True)
